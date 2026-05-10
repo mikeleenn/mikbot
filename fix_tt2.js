1648c3a6534d42cf7ac47752922053c29ca76a5c
mikelennn <mikeleenn@gmail.com>
2026-05-09 22:27:32 -0600

    Limpio - sin sesion

--- fix_tt2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let a = fs.readFileSync('bot.js','utf8');
-a = a.replace(
-'const link=args.slice(1).join(''');
-',
-"const link=texto.slice(4);"
-);
-fs.writeFileSync('bot.js', a);
-console.log('✅ /tt arreglado con texto.slice');
